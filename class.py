0fa2853bb0a2a2746bed6957e8b82a4e1d836c68
Ahmed Ibrahim <eng.ahmedmohamed6@gmail.com>
2021-03-03 19:11:07 +0200

    Complete Python Guide

---
+++ class.py
@@ -0,0 +1,25 @@
+# class Person:
+#     """Create a new Person"""
+#     def __init__(self, fname, lname):
+#         self.fname = fname
+#         self.lname = lname
+#     def new(self):
+#         print("Hi, my name is " + self.fname)
+#         print("Hi, my name is " + self.lname)
+
+# newPerson = Person("Dony", "Beckar")
+# anotherOne = Person("first", "last")
+
+# newPerson.new()
+
+# newPerson.fname = "Cris"
+# newPerson.lname = "Ronaldo"
+
+# del newPerson
+#Displaying the output
+# print(newPerson)
+# print(newPerson.fname)
+# print(newPerson.lname)
+
+# print(anotherOne.fname)
+# print(anotherOne.lname)
